fe110ae3ece275539c309689db9a1dea857bdc0b
Nirajan063 <nirajansingh632@gmail.com>
2025-10-24 14:59:23 +0545

    Add files via upload

---
+++ JavaScript series/01_variables.js
@@ -0,0 +1,6 @@
+const accountId= 1234
+let accountEmail = "nirajan123@gmail,com"
+let accountPassword = "6767"
+let accountAddress ;
+
+console.table([accountId,accountEmail,accountPassword,accountAddress])
